1894f4b40c6789bc751d924859289a44a51af4cd
Priya Gupta <priyagupta14317@gmail.com>
2019-05-18 20:06:41 -0500

    Adding Home.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
 import "./Home.css";
-class Home extends Component {
+import { AgGridReact } from "ag-grid-react";
 
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      columnDefs: [
+        { headerName: "GroupID", field: "id" },
+        { headerName: "GroupName", field: "name" },
+        { headerName: "Description", field: "detail" }
+      ],
+      rowData: [
+        { id: "001", name: "Computer Science", detail: "Oh teriii chal gya" },
+        { id: "002", name: "Mechanical Engineering", detail: "Bewakof log" }
+      ]
+    };
+  }
   render() {
-    return <div class="topic-list">List of Groups</div>;
+    return (
+      <div
+        className="ag-theme-balham"
+        style={{
+          height: "400px",
+          width: "600px",
+          marginleft: "- 300px",
+          margintop: "50px"
+        }}
+      >
+        <AgGridReact
+          columnDefs={this.state.columnDefs}
+          rowData={this.state.rowData}
+        />
+      </div>
+    );
   }
 }
 
